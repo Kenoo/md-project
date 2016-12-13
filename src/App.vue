6cf90d00084a90d8e6a670f4164c64311e8c8a66
kenoo <312008981@qq.com>
2016-12-14 01:07:36 +0800

    add first component for page

--- src/App.vue
+++ src/App.vue
@@ -27,12 +27,8 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
-          <ul class="nav nav-sidebar">
-            <li class="active"><a href="#">Overview</a></li>
-            <li><a href="#">Reports</a></li>
-            <li><a href="#">Analytics</a></li>
-            <li><a href="#">Export</a></li>
-          </ul>
+          <side-bar></side-bar>
+
         </div>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
             right
@@ -44,12 +40,16 @@
 </template>
 
 <script>
+import SideBar from  './components/sidebar.vue'
 export default {
   name: 'app',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  components: {
+    'side-bar': SideBar
   }
 }
 </script>
---
+++ src/components/sidebar.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <ul class="nav nav-sidebar">
+    <li class="active"><a href="#">Overview</a></li>
+    <li><a href="#">Reports</a></li>
+    <li><a href="#">Analytics</a></li>
+    <li><a href="#">Export</a></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data () {
+    return {};
+  }
+}
+</script>
+
+<style lang="css">
+</style>
